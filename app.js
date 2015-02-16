14a9d22998b5c70a4a6bf3f1f8c08c88f62a3d05
Alon Dahari <climbinghobo@gmail.com>
2015-02-16 16:05:17 -0700

    add success on form submit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var indexController = require('./controllers/index.js');
+var successController = require('./controllers/success.js');
 
 var app = express();
 app.set('view engine', 'jade');
@@ -9,6 +10,10 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', indexController.index);
+app.get('/success', successController.success);
+app.post('/formsubmit', function(req, res){
+	res.redirect('/success');
+});
 
 var server = app.listen(5264, function() {
 	console.log('Express server listening on port ' + server.address().port);
---
+++ controllers/success.js
@@ -0,0 +1,5 @@
+module.exports = {
+	success: function(req, res) {
+		res.render('success');
+	}
+};
